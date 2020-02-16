792159c305d4931c25289314a2ee28c6258bc3d7
Henry <hxmai@ucdavis.edu>
2020-02-15 17:32:28 -0800

    Changed main.py for easier testing in Pycharm

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import sys
 from BattleShip.src import game
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Not enough arguments given.')
-    else:
-        game_of_battle_ship = game.Game(sys.argv[1])
-        game_of_battle_ship.play()
+#    if len(sys.argv) < 2:
+#        print('Not enough arguments given.')
+#    else:
+#        game_of_battle_ship = game.Game(sys.argv[1])
+#        game_of_battle_ship.play()
+
+    file = "BattleShip/configs/mini_game.txt"
+    game_of_battle_ship = game.Game(file)
+    game_of_battle_ship.play()
